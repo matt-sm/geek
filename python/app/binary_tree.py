55369707f88d806052a57baece9fa249fa91f83d
Matt <matt.smith@digital.gov.au>
2018-07-12 09:40:24 +1000

    trasverse tree

--- python/app/binary_tree.py
+++ python/app/binary_tree.py
@@ -5,6 +5,18 @@ class BNode:
         self.right = None
         self.val = val
 
+    def __str__(self):
+        ''' Preorder traversal
+        '''
+        s = '{}{}'.format(str(self.val), ' ')
+        if self.left is not None:
+            s += str(self.left)
+
+        if self.right is not None:
+            s += str(self.right)
+
+        return s
+
     def insert(self, node):
         if node.val < self.val:
             if self.left is None:
--- python/tests/test_binary_tree.py
+++ python/tests/test_binary_tree.py
@@ -8,3 +8,16 @@ def test_insert_nodes():
 
     assert tree.left.val == 1
     assert tree.right.val == 3
+
+
+def test_preorder_traversal(capfd):
+    tree = BNode(10)
+    tree.insert(BNode(2))
+    tree.insert(BNode(12))
+    tree.insert(BNode(1))
+    tree.insert(BNode(11))
+    tree.insert(BNode(4))
+
+    print tree
+    out, err = capfd.readouterr()
+    assert out == '10 2 1 4 12 11 \n'
