725e1d1445ce7ba5e1a56ef6a374c12ff464a335
Matt <mattysmith@gmail.com>
2018-08-07 10:14:53 +1000

    sum values in array of objects

--- javascript/reduce.js
+++ javascript/reduce.js
@@ -1,5 +1,11 @@
-module.exports.sum = (arr) => {
+module.exports.sum = arr => {
 	return arr.reduce((accumulator, currentValue) => {
 		return accumulator + currentValue
+	}, 0)
+}
+
+module.exports.sumObj = arr => {
+	return arr.reduce((accumulator, currentValue) => {
+		return accumulator + Object.values(currentValue)[0]
 	}, 0)
 }
--- javascript/reduce.test.js
+++ javascript/reduce.test.js
@@ -4,3 +4,8 @@ test('sum array values', () => {
   expect(reduce.sum([1,2,3])).toBe(6)
   expect(reduce.sum([])).toBe(0)
 })
+
+test('sum object array', () => {
+  expect(reduce.sumObj([{x:1}, {y:2}, {z:3}])).toBe(6)
+  expect(reduce.sumObj([])).toBe(0)
+})
